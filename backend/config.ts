ca5e0b4ce87340b99eac8ce919f51fdc8136c986
Kash Saeed <kashifnizami@hotmail.com>
2020-12-04 16:01:29 +0100

    afte initialize MDB

--- backend/config.ts
+++ backend/config.ts
@@ -10,5 +10,5 @@ export const config = {
   env: process.env.SZ_ENV,
   appName: process.env.SZ_APP,
   port: process.env.SZ_PORT,
-  mongoUri: `mongodb+srv://${process.env.SZ_MONGO_USER}:${process.env.SZ_MONGO_PASS}@${process.env.SZ_MONGO_HOST}/${process.env.SZ_MONGO_DB}?authSource=admin&replicaSet=${process.env.SZ_MONGO_REPLICA}&readPreference=primary&ssl=true`,
+  mongoUri: `mongodb+srv://service:school123@schoolzone.vyian.mongodb.net/test?authSource=admin&replicaSet=atlas-nn67ik-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
 };
